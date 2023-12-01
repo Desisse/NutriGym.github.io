7d062da27547ffa5a91ca5a9ed35ee028d719f38
Anava300102 <Angel.ene02@hotmail.com>
2023-12-01 13:42:21 -0600

    Agregué el archivo Json para el log in y usuario

--- script.js
+++ script.js
@@ -13,3 +13,41 @@ function acceptCookies() {
     // Ocultar el cuadro de notificación de cookies
     document.getElementById("cookie-notification").style.display = "none";
 }
+
+function iniciarSesion() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    const storedUsers = JSON.parse(localStorage.getItem("users")) || [];
+
+    const userExists = storedUsers.some(user => user.username === username && user.password === password);
+
+    if (userExists) {
+        alert("Inicio de sesión exitoso");
+        window.location.href = "index.html";
+    } else {
+        alert("Nombre de usuario o contraseña incorrectos");
+    }
+}
+
+function registrarUsuario() {
+    const registroUsername = document.getElementById("registroUsername").value;
+    const email = document.getElementById("email").value;
+    const registroPassword = document.getElementById("registroPassword").value;
+
+    const storedUsers = JSON.parse(localStorage.getItem("users")) || [];
+
+    const userExists = storedUsers.some(user => user.username === registroUsername);
+
+    if (userExists) {
+        alert("Este nombre de usuario ya está en uso. Por favor, elige otro.");
+    } else {
+        const newUser = { username: registroUsername, email: email, password: registroPassword };
+        storedUsers.push(newUser);
+
+        localStorage.setItem("users", JSON.stringify(storedUsers));
+
+        alert("Usuario registrado exitosamente");
+        window.location.href = "index.html";
+    }
+}
